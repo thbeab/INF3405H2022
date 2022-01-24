2b1f931c1df8787b461c3747b03124aa956b86ad
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2022-01-24 18:39:02 -0500

    ClientHandler class

---
+++ src/ClientHandler.java
@@ -0,0 +1,40 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ClientHandler extends Thread
+{
+    private Socket socket;
+    private int clientNumber;
+
+    public ClientHandler(Socket socket, int clientNumber)
+    {
+        this.socket = socket;
+        this.clientNumber = clientNumber;
+        System.out.println("New connection with client# " + clientNumber + " at " + socket);
+    }
+    public void run()
+    {
+        try
+        {
+            DataOutputStream out = new DataOutputStream((socket.getOutputStream()));
+        } catch (IOException e)
+        {
+            System.out.println("Error handling client# " + clientNumber + ": " + e);
+
+        }
+        finally
+        {
+            try
+            {
+                socket.close();
+            }
+            catch(IOException e){
+                System.out.println("Couldn't close  a socket, what's going on?");
+            }
+            System.out.println("Connection with client# " + clientNumber + " closed");
+        }
+    }
+}
